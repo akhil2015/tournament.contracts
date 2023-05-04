579fa21f7fd11ff355302da7a5fd02f8936f4e3f
Akhil Bharti <akhil.bh96@gmail.com>
2023-05-04 17:01:56 +0530

    intial commit

---
+++ contracts/Tournament.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Tournament {
+    mapping(uint256 => address[]) public rosters;
+    mapping(uint256 => mapping(address=>bool)) public isPlayer;
+    mapping(uint256 => uint8) public isActive; //0 = not active, 1 = lobby,2 = active, 3= ended
+    mapping(uint256 => mapping(address => uint256)) public scores;
+    // mapping(uint256 => uint256) public prizePool;
+    // mapping(uint256 => uint256) public entryFee;
+    mapping(uint256 => uint256) public maxPlayers;
+
+    uint256 public tournamentCounter = 0;
+    event TournamentCreated(uint256 tournamentId);
+    event TournamentJoined(uint256 tournamentId, address player);
+    event TournamentStarted(uint256 tournamentId);
+    event TournamentEnded(uint256 tournamentId);
+    address public ORGANISER;
+
+    modifier onlyOrganiser() {
+        require(
+            ORGANISER == msg.sender,
+            "Only organiser can call this function"
+        );
+        _;
+    }
+
+    constructor(address _organiser)  {
+        ORGANISER = _organiser;
+    }
+
+    function createTournament(uint256 _maxPlayers) public onlyOrganiser {
+        maxPlayers[tournamentCounter] = _maxPlayers;
+        isActive[tournamentCounter] = 1;
+        emit TournamentCreated(tournamentCounter);
+        tournamentCounter++;
+    }
+
+    function joinTournament(uint256 _tournamentId) external {
+        require(isActive[_tournamentId] == 1, "Tournament is not available to join");
+        rosters[_tournamentId].push(msg.sender);
+        if(rosters[_tournamentId].length == maxPlayers[_tournamentId]){
+            isActive[_tournamentId] = 2;
+            emit TournamentStarted(_tournamentId);
+        }
+        emit TournamentJoined(_tournamentId, msg.sender);
+    }
+
+    function endTournament(
+        uint256 _tournamentId,
+        address[] calldata _users,
+        uint256[] calldata _scores
+    ) external onlyOrganiser {
+        require(isActive[_tournamentId] == 2, "Tournament is not active");
+        for (uint256 i = 0; i < _users.length; i++) {
+            scores[_tournamentId][_users[i]] = _scores[i];
+        }
+        isActive[_tournamentId] = 3;
+        emit TournamentEnded(_tournamentId);
+    }
+
+    function getRoster(
+        uint256 _tournamentId
+    ) public view returns (address[] memory) {
+        return rosters[_tournamentId];
+    }
+
+    function getScore(
+        uint256 _tournamentId,
+        address _user
+    ) public view returns (uint256) {
+        return scores[_tournamentId][_user];
+    }
+}
---
+++ test/Tournament.js
@@ -0,0 +1,107 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+
+describe("Tournament", function () {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshot in every test.
+  async function deployTournamentFixture() {
+    // Contracts are deployed using the first signer/account by default
+    const [owner, account1, account2, account3, account4] =
+      await ethers.getSigners();
+
+    const Tournament = await ethers.getContractFactory("Tournament");
+    const tournament = await Tournament.deploy(owner.address);
+
+    return { tournament, owner, account1, account2, account3, account4 };
+  }
+
+  describe("Deployment", function () {
+    it("Should set the organiser", async function () {
+      const { tournament, owner } = await loadFixture(deployTournamentFixture);
+
+      expect(await tournament.ORGANISER()).to.equal(owner.address);
+    });
+  });
+
+  describe("Create", function () {
+    it("Should create a tournament", async function () {
+      const { tournament, owner } = await loadFixture(deployTournamentFixture);
+      const MAX_PLAYERS = 5;
+      expect(await tournament.connect(owner).createTournament(MAX_PLAYERS))
+        .to.emit(tournament, "TournamentCreated")
+        .withArgs(0, owner.address, MAX_PLAYERS);
+    });
+    it("Only Organiser is able to create tournaments", async function () {
+      const { tournament, account1 } = await loadFixture(
+        deployTournamentFixture
+      );
+      const MAX_PLAYERS = 5;
+      await expect(
+        tournament.connect(account1).createTournament(MAX_PLAYERS)
+      ).to.be.revertedWith("Only organiser can call this function");
+    });
+  });
+  describe("Join", function () {
+    it("Should join a tournament", async function () {
+      const { tournament, owner, account1 } = await loadFixture(
+        deployTournamentFixture
+      );
+      const MAX_PLAYERS = 5;
+      await tournament.connect(owner).createTournament(MAX_PLAYERS);
+      expect(await tournament.connect(account1).joinTournament(0))
+        .to.emit(tournament, "TournamentJoined")
+        .withArgs(0, account1.address);
+    });
+    it("Should start the tournament once MAX_PLAYERS joined", async function () {
+      const { tournament, owner, account1, account2, account3 } =
+        await loadFixture(deployTournamentFixture);
+      const MAX_PLAYERS = 3;
+      await tournament.connect(owner).createTournament(MAX_PLAYERS);
+      await tournament.connect(account1).joinTournament(0);
+      await tournament.connect(account2).joinTournament(0);
+      expect(await tournament.connect(account3).joinTournament(0))
+        .to.emit(tournament, "TournamentStarted")
+        .withArgs(0);
+    });
+    it("Cannot join after tournament has started", async function () {
+      const { tournament,owner, account1,account2,account3,account4 } = await loadFixture(
+        deployTournamentFixture
+      );
+      const MAX_PLAYERS = 3;
+      await tournament.connect(owner).createTournament(MAX_PLAYERS);
+      await tournament.connect(account1).joinTournament(0);
+      await tournament.connect(account2).joinTournament(0);
+      await tournament.connect(account3).joinTournament(0);
+      await expect(
+        tournament.connect(account4).joinTournament(0)
+      ).to.be.revertedWith("Tournament is not available to join");
+    });
+  });
+  describe("End", function () {
+    it("Should end a tournament", async function () {
+      const { tournament, owner, account1, account2, account3 } =
+        await loadFixture(deployTournamentFixture);
+      const MAX_PLAYERS = 3;
+      await tournament.connect(owner).createTournament(MAX_PLAYERS);
+      await tournament.connect(account1).joinTournament(0);
+      await tournament.connect(account2).joinTournament(0);
+      await tournament.connect(account3).joinTournament(0);
+      expect(
+        await tournament
+          .connect(owner)
+          .endTournament(
+            0,
+            [account1.address, account2.address, account3.address],
+            [4, 7, 2]
+          )
+      )
+        .to.emit(tournament, "TournamentEnded")
+        .withArgs(0);
+    });
+  });
+});
